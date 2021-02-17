ad7fc2aa9fddce94a951d8afe157afe04731535e
Vulture68 <brice.kessler@hotmail.com>
2021-02-17 20:33:03 +0100

    Fixing the entity representation

--- src/main/java/fr/vulture/hostocars/entity/Operation.java
+++ src/main/java/fr/vulture/hostocars/entity/Operation.java
@@ -5,7 +5,7 @@ import static javax.persistence.GenerationType.IDENTITY;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import java.io.Serializable;
-import java.util.List;
+import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -17,6 +17,8 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.EqualsAndHashCode.Include;
 
 /**
  * Entity for the {@code operations} table.
@@ -24,11 +26,13 @@ import lombok.Data;
 @Entity
 @Data
 @Table(name = "operations")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 class Operation implements Serializable {
 
     private static final long serialVersionUID = -6271290610333034638L;
 
     @Id
+    @Include
     @GeneratedValue(strategy = IDENTITY)
     @Column(name = "id", unique = true, nullable = false, insertable = false, updatable = false, columnDefinition = "INTEGER")
     private Integer id;
@@ -36,13 +40,13 @@ class Operation implements Serializable {
     @Column(name = "label", nullable = false, columnDefinition = "TEXT")
     private String label;
 
-    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinColumn(name = "interventionId", referencedColumnName = "id")
     @JsonBackReference
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "interventionId", referencedColumnName = "id")
     private Intervention intervention;
 
-    @OneToMany(mappedBy = "operation", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JsonManagedReference
-    private List<OperationLine> operationLines;
+    @OneToMany(mappedBy = "operation", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    private Set<OperationLine> operationLines;
 
 }
